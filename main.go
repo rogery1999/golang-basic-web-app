eb08fe13f656f8e71aab86f7c4cd5d6a1d650af8
Rogery Vargas <rogery1999@hotmail.com>
2023-02-15 22:35:26 -0500

    feat: template rendering added

--- main.go
+++ main.go
@@ -1,85 +1,39 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"text/template"
 )
 
 const Port_Number = "8090"
 const Host_Address = "127.0.0.1"
 
-type DividePageRequestBody struct {
-	FirstOperator  float32 `json:"x"`
-	SecondOperator float32 `json:"y"`
-}
-
 // HomePageHandler is the home page handler
 func HomePageHandler(w http.ResponseWriter, r *http.Request) {
-	message := "This is the Home page"
-	_, err := fmt.Println(message)
-	if err != nil {
-		fmt.Println(err)
-	}
-	w.Header().Add("x-rogery-header", "rogery-vargas-home-page")
-	w.Write([]byte(message))
+	renderTemplate(w, r, "home.html")
 }
 
 // AboutPageHandler is the about page handler
 func AboutPageHandler(w http.ResponseWriter, r *http.Request) {
-	mySum := addValue(2, 2)
-	message := fmt.Sprintf("This is the About Page, and 2 + 2 is %d", mySum)
-	_, err := fmt.Println(message)
-	if err != nil {
-		fmt.Println(err)
-	}
-	w.Header().Add("x-rogery-header", "rogery-vargas-about-page")
-	w.Write([]byte(message))
+	renderTemplate(w, r, "about.html")
 }
 
-// addValue add two values and return the sum
-func addValue(x, y int) int {
-	return x + y
-}
+func renderTemplate(w http.ResponseWriter, r *http.Request, templateName string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + templateName)
 
-func DividePageHandler(w http.ResponseWriter, r *http.Request) {
-	bodyBytes, err := ioutil.ReadAll(r.Body)
+	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "an error occurred while trying to read the request's body")
+		fmt.Println("error parsing template", err)
 		return
 	}
-	requestBody := DividePageRequestBody{}
-	json.Unmarshal(bodyBytes, &requestBody)
-
-	f, err := divideValues(requestBody.FirstOperator, requestBody.SecondOperator)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "Cannot divide by 0")
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "The result of divide %f / %f is %f", requestBody.FirstOperator, requestBody.SecondOperator, f)
-}
-
-func divideValues(x, y float32) (float32, error) {
-	if y == 0 {
-		err := errors.New("cannot divide by zero")
-		return 0, err
-	}
-	result := x / y
-	return result, nil
 }
 
 func main() {
 	http.HandleFunc("/", HomePageHandler)
 	http.HandleFunc("/about", AboutPageHandler)
-	http.HandleFunc("/divide", DividePageHandler)
 
 	baseUrl := fmt.Sprintf("%v:%v", Host_Address, Port_Number)
 	fmt.Printf("Trying to run the server on: http://%v\n", baseUrl)
