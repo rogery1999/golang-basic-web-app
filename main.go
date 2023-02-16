919118bf3f2e89c146c934cb1fbb105c2dfd6667
Rogery Vargas <rogery1999@hotmail.com>
2023-02-15 22:49:54 -0500

    feat: business logic reorganized

--- main.go
+++ main.go
@@ -5,31 +5,10 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"text/template"
 )
 
 const Port_Number = "8090"
 const Host_Address = "127.0.0.1"
-
-// HomePageHandler is the home page handler
-func HomePageHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, r, "home.html")
-}
-
-// AboutPageHandler is the about page handler
-func AboutPageHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, r, "about.html")
-}
-
-func renderTemplate(w http.ResponseWriter, r *http.Request, templateName string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + templateName)
-
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template", err)
-		return
-	}
-}
 
 func main() {
 	http.HandleFunc("/", HomePageHandler)
